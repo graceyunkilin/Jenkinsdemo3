ce7e57d4b358b3e06c7b648120c3b8dcd25528a5
graceyukilin <graceyukilin03@outlook.com>
2023-07-26 02:24:01 +0800

    111

---
+++ src/main/java/com/test/jenkinsdemo3/Jenkinsdemo3Application.java
@@ -0,0 +1,13 @@
+package com.test.jenkinsdemo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkinsdemo3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jenkinsdemo3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/jenkinsdemo3/Jenkinsdemo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.jenkinsdemo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jenkinsdemo3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
